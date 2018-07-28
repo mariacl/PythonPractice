d6d46d6fba8ad0c8e0a3f2343d75a2efb5721d16
mariacl <mariacl@users.noreply.github.com>
2018-07-28 22:42:10 +0100

    with GUI

---
+++ replace.py
@@ -0,0 +1,72 @@
+# Replace a substring
+
+from tkinter import *
+import re
+
+main = Tk()
+main.title("Replace Stuff 1.0")
+main.geometry("400x400")
+
+
+
+#Subrutinas
+
+
+def replace ():
+    
+    inputf = entryBoxInputFile.get()
+    outputf = entryBoxOutputFile.get()
+    reg = entryBoxReg.get()
+    subst = entryBoxReplaceWith.get()
+
+
+    input_file = open(inputf, "r")
+    output_file = open(outputf, "w")
+    
+
+    lookfor = re.compile(reg) # string pattern we are looking for. If you want the level separator / you need to include it, because .* won't match it.
+    replacewith = subst #substring replacement
+
+    for line in input_file:
+        match = re.search(lookfor, line) # we put en-us html finds in a variable
+        if match:
+            replaced = re.sub('en-us', replacewith, line) # make ths substitution on found matches
+            output_file.write(replaced) # write line with the replacement
+        else:
+            output_file.write(line) # write any other no matching lines
+
+
+
+#Frames/labels
+Label(main, text="Replace en-us 1.0", font=16).grid(row=0, column=0, sticky=W)
+
+
+#EntryBox
+
+entryBoxInputFile = Entry(main, width=20, bg="light grey")
+entryBoxInputFile.grid(row=3, column=0, sticky=W)
+
+entryBoxOutputFile =  Entry(main, width=20, bg="light grey")
+entryBoxOutputFile.grid(row=4, column=0, sticky=W)
+
+entryBoxReg = Entry(main, width=20, bg="light grey")
+entryBoxReg.grid(row=5, column=0, sticky=W)
+
+# entryBoxSubs = Entry(main, width=20, bg="light grey") In case we want to give opportunity to enter something other than en-us
+# entryBoxSubs.grid(row=3, column=0, sticky=W)
+
+entryBoxReplaceWith = Entry(main, width=20, bg="light grey")
+entryBoxReplaceWith.grid(row=6, column=0, sticky=W)
+
+
+
+
+
+#Importing buttons
+
+ReplaceButton = Button(main, text="Replace", width=30, command = replace)
+ReplaceButton.grid(row=7, column=10, sticky=W)
+
+main.mainloop()
+# ==============================
+
