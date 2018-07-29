6023c733d01d76c71f271b9754ccbd8f2f14db4a
mariacl <mariacl@users.noreply.github.com>
2018-07-29 07:55:10 +0100

    basic gui

--- firstGUI.py
+++ firstGUI.py
@@ -5,14 +5,17 @@ from tkinter import messagebox
 main = Tk()
 
 main.title("Replace Stuff 1.0")
-main.geometry("400x500")
+main.geometry("400x400")
 
 def copy():
     temp = inputBox.get()
     output.insert(END, temp)
+    inputBox.delete(0,END)
+
 inputBox = Entry(main, width=20, bg="light grey")
 inputBox.grid(row=0, column=0, sticky=W)
 
+# we need to define the grid for input,output, button etc. Below, row=1 because 0 is taken by inputbox
 Button(main, width=10, text="Copiar", command = copy).grid(row=1, column=0, sticky =W)
 
 output = Text(main, width=20, height=5, background="light grey")
